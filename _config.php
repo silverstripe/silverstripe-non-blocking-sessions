a1f2d2ad24889f208ef5c819287cd33f2c85c3d7
Steve Boyd <emteknetnz@gmail.com>
2025-11-17 13:49:01 +1300

    DOC Add composer install command and repository link

--- _config.php
+++ _config.php
@@ -5,11 +5,14 @@ use SilverStripe\Core\Injector\Injector;
 use SilverStripe\NonBlockingSessions\FileSessionHandler;
 
 // PHPUnit 9 outputs headers early. This is unavoidable with PHPUnit 9.
-// We can't use PHPUnit 10+ because of a dependency conflict with `sebastian/diff` which is a dependency of `silverstripe/framework`.
-// After headers have been sent, we can't call `session_set_save_handler()` anymore without PHP complaining about it, so we
+// We can't use PHPUnit 10+ because of a dependency conflict with `sebastian/diff` which is
+// a dependency of `silverstripe/framework`.
+// After headers have been sent, we can't call `session_set_save_handler()` anymore without
+// PHP complaining about it, so we
 // need to avoid calling that method after headers have been sent.
 if (headers_sent($filename, $line)) {
-    // since we're handling this scenario here we should log it just in case something weird happens during non-test execution
+    // since we're handling this scenario here we should log it just in case something weird
+    // happens during non-test execution
     Injector::inst()->get(LoggerInterface::class)->warning(sprintf(
         'Session save handler cannot be changed after headers have already been sent in %s on line %s',
         $filename,
